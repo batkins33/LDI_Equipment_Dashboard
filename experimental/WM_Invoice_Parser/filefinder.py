a177c4fa0a8d259b22e29cd9d1f2e81c1bab244f
batkins33 <142167300+batkins33@users.noreply.github.com>
2025-12-08 13:57:21 -0600

    Cleanup: remove migrated files, consolidate to manifest_validation project

--- experimental/WM_Invoice_Parser/filefinder.py
+++
@@ -1,6 +0,0 @@
-import os
-
-folder = r"C:\Users\brian.atkins\OneDrive - Lindamood Demolition\24-105 PHMS NPC - Documents\PM\Invoices\invoices"
-
-for f in os.listdir(folder):
-    print(repr(f))
--- manifest_extraction_toolkit/src/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Utilities for combining multiple manifest PDFs into a single document."""
-
-from .main import combine_pdfs
-
-__all__ = ["combine_pdfs"]
